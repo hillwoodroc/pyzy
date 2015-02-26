344872231b95ce7c2808288101cc563dcd011940
Hillwood Yang <hillwoodroc@gmail.com>
2015-02-26 11:04:29 +0800

    Update SimpTradConverter.cc
    Use opencc 1.0.2

--- src/SimpTradConverter.cc
+++ src/SimpTradConverter.cc
@@ -4,20 +4,21 @@
  *
  * Copyright (c) 2008-2010 Peng Huang <shawn.p.huang@gmail.com>
  * Copyright (c) 2010 BYVoid <byvoid1@gmail.com>
+ * Copyright (c) 2015 Hillwood Yang <hillwood@opensuse.org>
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * This library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of the
+ * License, or (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * This library is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
  * USA
  */
 #include "SimpTradConverter.h"
@@ -40,7 +41,6 @@ namespace PyZy {
 #ifdef HAVE_OPENCC
 
 class opencc {
-    static const int BUFFER_SIZE = 64;
 public:
     opencc (void)
     {
@@ -55,29 +55,13 @@ public:
 
     void convert (const char *in, String &out)
     {
-        long n_char;
-        unichar *in_ucs4 = g_utf8_to_ucs4_fast (in, -1, &n_char);
-
-        ucs4_t *pinbuf = (ucs4_t *)in_ucs4;
-        size_t inbuf_left = n_char;
-        while (inbuf_left != 0) {
-            ucs4_t *poutbuf = (ucs4_t *)m_buffer;
-            size_t outbuf_left = BUFFER_SIZE;
-            size_t retval = opencc_convert(m_od, &pinbuf, &inbuf_left, &poutbuf, &outbuf_left);
-            if (retval == (size_t) -1) {
-                /* append left chars in pinbuf */
-                g_warning ("opencc_convert return failed");
-                out << (unichar *) pinbuf;
-                break;
-            }
-            *poutbuf = L'\0';
-            out << m_buffer;
-        }
-        g_free (in_ucs4);
+        char * converted = opencc_convert_utf8 (m_od, in, -1);
+        g_assert (converted != NULL);
+        out = converted;
+        opencc_convert_utf8_free (converted);
     }
 private:
     opencc_t m_od;
-    unichar m_buffer[BUFFER_SIZE + 1];
 };
 
 void
